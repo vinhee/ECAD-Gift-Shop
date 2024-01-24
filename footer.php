7b614c2215e7a1edbe19e5dedb0e9bd08fe791e1
vinhee <S10221118@connect.np.edu.sg>
2024-01-24 12:17:54 +0800

    Updated Nav Bar

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<!--Done by: Vin Hee-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +10,13 @@
 </head>
 <body>
 
-<footer class="py-3 my-4" style="background-color:#638889; color:white;">
+<footer class="py-3 my-4" style="background-color:#C7B7A3; color:black;">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item foot-text"><a href="index.php" class="nav-link px-2 text-body-secondary" style="color:white;">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:white;">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:white;">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:white;">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:white;">About</a></li>
+      <li class="nav-item foot-text"><a href="index.php" class="nav-link px-2 text-body-secondary" style="color:black;">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:black;">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:black;">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:black;">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary" style="color:black;">About</a></li>
     </ul>
     <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
   </footer>
--- navbar.php
+++ navbar.php
@@ -6,30 +6,34 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/bootstrap.bundle.min.js"></script>
-    <title>Memorable Gifts</title>
+    <script src="https://kit.fontawesome.com/624113c1c9.js" crossorigin="anonymous"></script>
+    <title>GiftMaven</title>
 </head>
 
-<nav class="navbar navbar-expand navbar-dark" style="background-color:#638889">
-  <a class="navbar-brand" href="index.php">Page Icon</a>
+<nav class="navbar navbar-expand navbar-light" style="background-color: #C7B7A3">
+  <a class="navbar-brand" href="index.php"><img src="Images/GiftMaven. logo.png" height="100px";></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+        <a class="nav-link" href="index.php" style="font-size: 18px;">Home <span class="sr-only"></span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="productCat.php">Categories</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="productCat.php" style="font-size: 18px;">Categories<span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="font-size: 18px;">Search<span class="sr-only"></span></a>
       </li>
     </ul>
   </div>
 
-  <div class="d-flex align-items-center px-3" style="color:white">
-          Shopping Cart
-        </div>
+  <div class="d-flex align-items-center px-3" style="font-size: 22px;">
+    <a class="nav-link active" href="#" style="color:black;"><i class="fa-solid fa-cart-shopping"></i></a>
+  </div>
 
-  <div id="sign-up" class="d-flex align-items-center px-3" style="color:white">
-    Sign Up
+  <div id="sign-up" class="d-flex align-items-center px-3" style="font-size: 22px;">
+    <a class= "nav-link active" href="#" style="color:black;"><i class="fa-solid fa-user"></i></a>
   </div>
 </nav>
